92059e92cbc35a6991cd642cdc53bf1ab88e7f5a
kevmsutton <kevmsutton@hotmail.co.uk>
2018-10-11 09:51:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function takeANumber(currentLine, newName){
 currentLine.push(newName)
 return (`Welcome, ${newName}. You are number ${currentLine.length} in line.`)
 }
+
+function nowServing(currentLine){
+let array = []
+for (let i = 0; i < currentLine.length; i++){
+array.push(currentLine[i])
+let firstPerson = array.shift(currentLine[i])
+return (firstPerson)
+}
+}
+console.log(nowServing(["John", "Becky", "Jimmy"]))
